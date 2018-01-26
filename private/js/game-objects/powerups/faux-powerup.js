9c1dfdef1f0f09a512ad626f62ffe98438f4f4e6
jakeb994 <jakeb994@gmail.com>
2018-01-27 01:54:00 +1300

    Add fake powerup

---
+++ private/js/game-objects/powerups/faux-powerup.js
@@ -0,0 +1,51 @@
+const GameObject = require('../game-object');
+const collision = require("../../utilities/collision");
+
+class FauxPowerup extends GameObject {
+    constructor(
+        game,
+        x,
+        y,
+        direction,
+        sprite,
+        collisionRadius,
+        duration,
+        healthDecrease
+    ) {
+        super(game, x, y, direction, sprite, collisionRadius);
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.sprite = sprite;
+        this.collisionRadius = collisionRadius;
+        this.duration = duration;
+        this.healthDecrease = healthDecrease;
+        this.type = 'FauxPowerup';
+    }
+
+    loop(deltaTime, currentTime) {
+        super.loop(deltaTime, currentTime);
+        const collisions = collision.getCollisions(this.game, this.x, this.y, this.collisionRadius);
+        let i = collisions.length;
+        while (i--) {
+            if (collisions[i].id !== this.id) {
+                switch(collisions[i].type) {
+                    case 'Unit':
+                        this.onCollisionWithUnit(collisions[i]);
+                        break;
+                }
+            }
+        }
+
+        if (Math.round(currentTime) % 5 === 0) {
+            //this.sprite = bomb sprite;
+        } else {
+            //this.sprite = sheild powerup sprite
+        }
+    }
+
+    onCollisionWithUnit(unit) {
+        unit.health -= this.healthDecrease;
+    }
+}
